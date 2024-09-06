cbb593d533e67a03bf6942aa4ba309aae9486b49
manhvdo95 <manhvdo95@yahoo.com>
2024-09-05 23:27:28 -0500

    added icon

--- src/app/modules/angular-material/angular-material.module.ts
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   exports: [
     MatButtonModule,
-    MatTabsModule
+    MatTabsModule,
+    MatIconModule
   ]
 })
 export class AngularMaterialModule { }
